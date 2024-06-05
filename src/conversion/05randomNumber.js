93f516051355e16b62630334b5182e736521c932
2405-002-ABD <2405-002-abd@U-ZTCCGG14L2B8.corp.amazonworkspaces.com>
2024-06-05 15:59:21 +0100

    halfway through exercises

--- src/conversion/05randomNumber.js
+++ src/conversion/05randomNumber.js
@@ -1,9 +1,6 @@
 /**
  * @returns {number} a random number between 1 and 10 (inclusive)
  */
-function randomNumber() {
-    const fraction = Math.random(); //a number between 0 and 1 (exclusive)
-    return 1 + Math.floor(fraction * 10);
-}
+const randomNumber = () => Math.random();
 
 export { randomNumber };
